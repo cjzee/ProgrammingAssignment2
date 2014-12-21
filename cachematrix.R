7318b9f1f0eb00b03b91dd291ae725510781809d
cjzee <carleyj@gmail.com>
2014-12-21 09:41:51 -0800

    Programming Assignment 2 (R Programming) - documentation rewritten

--- cachematrix.R
+++ cachematrix.R
@@ -2,10 +2,13 @@
 ## cachematrix.R
 #==========================
 
+## 18 December 2014
 ## Functions to cache results from time-consuming computations.
-## Basic concept is to create a "make" function that stores cached objects in a list and a "cache" function that saves time by computing  the cached object(s) and storing them in the "make"object or retrieving them from the "make"object if already cached.
+## Basic concept is to create a "make" function that stores cached objects in a list and a "cache" function that saves time by computing 
+## the cached object(s) and storing them in the "make"object or retrieving them from the "make"object if already cached.
 
-## The "make" function can be called once to store the original object and placeholders for objects to be computed and set by the "cache" function (like mean, inverse matrix, etc)
+## The "make" function can be called once to store the original object and placeholders for objects to be computed and set by the "cache" function
+## (like mean, inverse matrix, etc)
 
 #==========================
 ## Sample data and commands
@@ -16,19 +19,23 @@
 ## minv$get()
 ## minv$getinverse() ## will return NULL the first time b/c the inverse of minv has not been cached yet
 ## cacheSolve(minv) ## first time called will compute the inverse, save it to makeCacheMatrix.getinverse() and return the inverse
-## cacheSolve(minv) ## second time called will figure out 
+## cacheSolve(minv) ## second time called will figure out the matrix has been cached and instead of computing it, return the cached matrix using getinverse()
 ## minv$get() %*% minv$getinverse() ## return the identity matrix from minv
 
-## "make" function that accepts a matrix and stores it and the inverse if called ither 
+## "make" function that accepts a matrix, storing the original and the inverse (when called by cacheSolve or the method "setinverse()") 
 ## Note: the inverse can be loaded directly without using caseSolve 
 ## To do this
 ## mm <- makeCacheMatrix(matrix(1:4,2,2))
-## mm$getinverse() ## returns NULL
+## mm$getinverse() ## returns NULL -- this line is to demonstrate the value of the inverse
 ## mm$setinverse(solve(m)) ## sets inverse
 ## mm$getinverse() ## returns cached inverse matrix
 #==========================
 
-## "make" function to store a matrix and it's inverse, with methods to get and set the inverse and get the original
+#==========================
+## MATRIX FUNCTIONS
+#==========================
+
+## "make" function to store a matrix and it's inverse, with methods to get/set the inverse and get the original
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y = matrix()) {
@@ -45,7 +52,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## "cache" function to get the cached inverse from the "make" object or to compute and set it if not found.
-
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
